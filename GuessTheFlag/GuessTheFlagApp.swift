0d8604834d0fe097734a989112ea0ddbb0697d6d
Rasika Punde <rasikapunde@gmail.com>
2024-01-08 12:23:57 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Pratik Mistry on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
